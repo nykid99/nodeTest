360c1d0e4a6212044f8562295f347f0d71d950fb
nykid99 <kecho@chapman.edu>
2023-04-15 21:50:28 -0500

    adjusted writeHead

--- index.js
+++ index.js
@@ -4,39 +4,43 @@ const port = 3000
 
 const server = http.createServer(function(req, res) {
     if(req.url === '/') {
-        res.writeHead(200 , { 'Content-Type' : 'text/html'})
+
         fs.readFile('index.html', function(error, data){
             if (error) {
                 res.writeHead(404)
                 res.write('Error: File Not Found')
             }
             else{
+                res.writeHead(200 , { 'Content-Type' : 'text/html'})
                 res.write(data)
             }
             res.end()
         })
     }
     else if(req.url === '/about') {
-        res.writeHead(200 , { 'Content-Type' : 'text/html'})
+
         fs.readFile('about.html', function(error, data){
             if (error) {
                 res.writeHead(404)
                 res.write('Error: File Not Found')
             }
             else{
+                res.writeHead(200 , { 'Content-Type' : 'text/html'})
                 res.write(data)
             }
             res.end()
         })
     }
     else if(req.url === '/contact-me') {
-        res.writeHead(200 , { 'Content-Type' : 'text/html'})
+
         fs.readFile('contact-me.html', function(error, data){
             if (error) {
+
                 res.writeHead(404)
                 res.write('Error: File Not Found')
             }
             else{
+                res.writeHead(200 , { 'Content-Type' : 'text/html'})
                 res.write(data)
             }
             res.end()
